e394b00da7ba3b61f22b47dbee0eb454ff8a6be9
Tomek <superklodziak@o2.pl>
2026-03-08 08:39:24 +0100

    Fixed interest type

---
+++ Mortgage.Api/Persistance/Migrations/20260308072818_2112202515.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mortgage.Migrations
+{
+    /// <inheritdoc />
+    public partial class _2112202515 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "First_Interest_Amount",
+                table: "Mortgages",
+                type: "decimal(18,2)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Interest_Rate_Type",
+                table: "Mortgages",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "First_Interest_Amount",
+                table: "Mortgages");
+
+            migrationBuilder.DropColumn(
+                name: "Interest_Rate_Type",
+                table: "Mortgages");
+        }
+    }
+}
